5bae8fa523f4c6a4077c99334b469a467d012407
tampler <socnetfpga@gmail.com>
2021-12-03 16:00:32 +0300

    Upd test

--- test/nws_subnet_vpc_test.go
+++ test/nws_subnet_vpc_test.go
@@ -12,21 +12,20 @@ import (
 func TestNwsSubnetVpcExample(t *testing.T) {
 	t.Parallel()
 
-	cfg := make(map[string]string)
-
-	cfg["name"] = fmt.Sprintf("terratest-subnet-%s", random.UniqueId())
-	cfg["cidr"] = "10.0.1.0/24"
-	cfg["domain"] = "alfa.local"
-	cfg["vpc_id"] = "aba8788b-ec7c-4e6f-a873-b38840e3c834"
+	const (
+		cidr       = "10.0.1.0/24"
+		exp_domain = "alfa.local"
+		vpc_id     = "aba8788b-ec7c-4e6f-a873-b38840e3c834"
+	)
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../examples/vpc",
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
-			"name":   cfg["name"],
-			"cidr":   cfg["cidr"],
-			"domain": cfg["domain"],
-			"vpc_id": cfg["vpc_id"],
+			"name":   fmt.Sprintf("terratest-subnet-%s", random.UniqueId()),
+			"cidr":   cidr,
+			"domain": exp_domain,
+			"vpc_id": vpc_id,
 		},
 	})
 
@@ -35,9 +34,9 @@ func TestNwsSubnetVpcExample(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	id := terraform.Output(t, terraformOptions, "id")
-	domain := terraform.Output(t, terraformOptions, "domain")
+	act_domain := terraform.Output(t, terraformOptions, "domain")
 
-	assert.Equal(t, cfg["domain"], domain)
+	assert.Equal(t, exp_domain, act_domain)
 	assert.NotEmpty(t, id)
 	assert.NotNil(t, id)
 }
